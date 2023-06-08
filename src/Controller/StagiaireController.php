86b9b9e7ebd665f90f9f2579dfa4ab10e26a0cf0
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-06-08 10:33:31 +0100

    stagiaire

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -4,13 +4,44 @@ namespace App\Controller;
 
 use App\Entity\Session;
 use App\Entity\Stagiaire;
+use App\Form\StagiaireType;
 use App\Repository\StagiaireRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StagiaireController extends AbstractController
 {
+
+
+
+#[Route('stagiaire/add', name: 'add_stagiaire')]
+#[Route('/stagiaire/{id}/edit', name: 'edit_stagiaire')]
+public function add(EntityManagerInterface $entityManager, Stagiaire $stagiaire = null, Request $request): Response{
+    
+    /* Si le stagiaire n'existe pas, on le crée */
+    if(!$stagiaire){
+        $stagiaire = new Stagiaire();
+    }
+    
+    $form = $this->createForm(StagiaireType::class, $stagiaire); // On crée le formulaire
+    $form->handleRequest($request); // Inspecte la requête
+    if($form->isSubmitted() && $form->isValid()){
+        $stagiaire = $form->getData(); // On récupère les données
+        $entityManager->persist($stagiaire); // Équivalent du prepare
+        $entityManager->flush(); // Équivalent du execute
+
+        return $this->redirectToRoute("app_stagiaire");
+    }
+
+    return $this->render('stagiaire/add.html.twig', [
+        'formAddStagiaire' => $form->createView(),
+        'edit' => $stagiaire->getId()
+    ]);
+}
     #[Route('/stagiaire', name: 'app_stagiaire')]
     public function index(StagiaireRepository $stagiaireRepository): Response
     {
