ed02b1ea53323f940c8d549ff922595dbbb9de5d
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-06-08 08:29:15 +0100

    delete stagiaire

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,14 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\Session;
+use App\Entity\Stagiaire;
 use App\Repository\ModuleRepository;
 use App\Repository\SessionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
  class SessionController extends AbstractController
 {
+   
+    #[Route('/stagiaire/{id}/delete', name : 'delete_stagiaire')]
+    public function delete(EntityManagerInterface $entityManager, Stagiaire $stagiaire = null): Response{
+        $nomStagiaire = $stagiaire->getPrenom() . " " . $stagiaire->getNom();
+        $entityManager->remove($stagiaire);
+        $entityManager->flush();
+        $this->addFlash('notice', 'Le stagiaire "' .  $nomStagiaire . '" a été supprimer');
+
+        return $this->redirectToRoute("app_stagiaire");
+    }
+      
     #[Route('/session', name: 'app_session')]
     public function index(SessionRepository $sessionRepository): Response
     
