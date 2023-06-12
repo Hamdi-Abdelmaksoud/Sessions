3339114ba168f1fdc2839cc586873073d50c113a
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-06-12 07:33:32 +0100

    add session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -4,16 +4,43 @@ namespace App\Controller;
 
 use App\Entity\Session;
 use App\Entity\Stagiaire;
+use App\Form\SessionType;
+
 use App\Repository\ModuleRepository;
 use App\Repository\SessionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
  class SessionController extends AbstractController
 {
    
+    #[Route('session/add', name: 'add_session')]
+#[Route('/session/{id}/edit', name: 'edit_session')]
+public function add(EntityManagerInterface $entityManager, Session $session = null, Request $request): Response{
+    
+    /* Si session n'existe pas, on le crée */
+    if(!$session){
+        $session = new Session();
+    }
+    
+    $form = $this->createForm(SessionType::class, $session); // On crée le formulaire
+    $form->handleRequest($request); // Inspecte la requête
+    if($form->isSubmitted() && $form->isValid()){
+        $session = $form->getData(); // On récupère les données
+        $entityManager->persist($session); // Équivalent du prepare
+      
+        $entityManager->flush(); // Équivalent du execute
+
+        return $this->redirectToRoute("app_session");
+    }
+
+    return $this->render('session/add.html.twig', [
+        'formAddSession' => $form->createView(),
+        'edit' => $session->getId()
+    ]);
+}
     #[Route('/stagiaire/{id}/delete', name : 'delete_stagiaire')]
     public function delete(EntityManagerInterface $entityManager, Stagiaire $stagiaire = null): Response{
         $nomStagiaire = $stagiaire->getPrenom() . " " . $stagiaire->getNom();
